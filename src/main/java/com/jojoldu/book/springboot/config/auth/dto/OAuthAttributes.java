4ee74b88649c0cdabce23c1ca7447c7bdcde0206
Juhyun Kim <rlawngus0910@naver.com>
2021-12-22 17:52:13 +0900

    네이버 로그인 추가

--- src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -26,6 +26,10 @@ public class OAuthAttributes {
 
     public static OAuthAttributes of(String registrationId, String userNameAttribute,
                                      Map<String, Object> attributes) {
+
+        if("naver".equals(registrationId)) {
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttribute, attributes);
     }
 
@@ -39,6 +43,17 @@ public class OAuthAttributes {
                 .build();
     }
 
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>)attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
     public User toEntity() {
         return User.builder()
                 .name(name)
